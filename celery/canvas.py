247e94c82a54a9074c6855348a5b51755961185d
Nils Caspar <nils@outset.ai>
2024-05-13 11:04:14 -0700

    Bugfix - a task will run multiple times when chaining chains with groups

--- celery/canvas.py
+++ celery/canvas.py
@@ -975,8 +975,7 @@ class _chain(Signature):
         elif isinstance(other, _chain):
             # chain | chain -> chain
             # use type(self) for _chain subclasses
-            return type(self)(seq_concat_seq(
-                self.unchain_tasks(), other.unchain_tasks()), app=self._app)
+            return reduce(operator.or_, other.unchain_tasks(), self)
         elif isinstance(other, Signature):
             if self.tasks and isinstance(self.tasks[-1], group):
                 # CHAIN [last item is group] | TASK -> chord
